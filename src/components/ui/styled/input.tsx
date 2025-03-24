c5bd8d9cede3acc35a99d33c8a5f9cd76130389c
Hasegawa Yuta <gssbtsm999@gmail.com>
2025-03-24 14:25:27 +0900

    refactor(input): data属性による error style の切替

--- src/components/ui/styled/input.tsx
+++ src/components/ui/styled/input.tsx
@@ -11,12 +11,10 @@ interface InputBaseProps {
   invalid?: boolean;
 }
 
-function createClassName(invalid?: boolean) {
-  return clsx(
-    "block w-full appearance-none rounded-md px-3 py-1 ring-1 ring-inset ring-base-light-300 placeholder:text-base-light-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary-600",
-    invalid && "ring-2 ring-danger-500 focus:ring-danger-500",
-  );
-}
+const baseClassName = clsx(
+  "block w-full appearance-none rounded-md px-3 ring-1 ring-inset ring-base-light-300 placeholder:text-base-light-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary-600",
+  "data-[invalid='true']:ring-2 data-[invalid='true']:ring-danger-500 data-[invalid='true']:focus:ring-danger-500",
+);
 
 // ----------------------------------------
 
@@ -27,7 +25,8 @@ const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
 
   return (
     <input
-      className={clsx("h-10", createClassName(invalid), className)}
+      data-invalid={invalid ? "true" : undefined}
+      className={clsx("h-10", baseClassName, className)}
       {...restProps}
       ref={ref}
     />
@@ -48,11 +47,8 @@ const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
 
     return (
       <textarea
-        className={clsx(
-          createClassName(invalid),
-          "py-3 leading-normal",
-          className,
-        )}
+        data-invalid={invalid ? "true" : undefined}
+        className={clsx(baseClassName, "py-3 leading-normal", className)}
         {...restProps}
         ref={ref}
       />
