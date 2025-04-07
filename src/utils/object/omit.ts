c73d6179c406079506d05faf6d7467401ad66950
Hasegawa Yuta <gssbtsm999@gmail.com>
2025-04-08 01:13:07 +0900

    refactor(omit): type-safe keys arg

--- src/utils/object/omit.ts
+++ src/utils/object/omit.ts
@@ -1,15 +1,11 @@
 function omit<T extends object, K extends string>(
   object: T,
-  keys: K[],
+  keys: Array<keyof T>,
 ): Omit<T, K> {
   const result = { ...object };
 
-  for (const key in object) {
-    if (Object.prototype.hasOwnProperty.call(object, key)) {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      if (keys.includes(key)) delete result[key];
-    }
+  for (const key of keys) {
+    delete result[key];
   }
 
   return result as Omit<T, K>;
