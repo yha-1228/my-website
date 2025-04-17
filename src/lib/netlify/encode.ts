c39cc91db2726370165b1b47b12a53432eea01f7
Hasegawa Yuta <gssbtsm999@gmail.com>
2025-04-18 03:44:04 +0900

    refactor: remove dict type

--- src/lib/netlify/encode.ts
+++ src/lib/netlify/encode.ts
@@ -1,10 +1,8 @@
-import { type Dict } from "@/types/utils";
-
 /**
  * @see https://www.netlify.com/blog/2017/07/20/how-to-integrate-netlifys-form-handling-in-a-react-app/
  */
 export const encode = (
-  data: Dict<Parameters<typeof encodeURIComponent>[0]>,
+  data: Record<string, Parameters<typeof encodeURIComponent>[0]>,
 ) => {
   return Object.keys(data)
     .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,7 +1,5 @@
 export type ValueOf<T> = T[keyof T];
 
-export type Dict<T = unknown> = Record<string, T>;
-
 export type Replace<T, U extends { [key in keyof T]?: unknown }> = Omit<
   T,
   keyof U
