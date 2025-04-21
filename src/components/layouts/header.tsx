9cbab41ae4c3b7408b83bc7b27ccc8a8b90d1f7b
Hasegawa Yuta <gssbtsm999@gmail.com>
2025-04-21 18:40:42 +0900

    refactor(header): refactor markup

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -73,75 +73,78 @@ export function Header() {
   };
 
   return (
-    <header ref={headerRef} className="z-header sticky top-0">
-      <div
-        style={
-          {
-            "--header-border-bottom-width": hederBorderBottomWidth,
-          } as CSSProperties
-        }
-        className="border-b-base-light-300 relative h-(--height-header) border-b-[length:var(--header-border-bottom-width)] border-solid bg-white"
-      >
-        <Container>
-          <div className="relative flex h-[calc(var(--height-header)-var(--header-border-bottom-width))] items-center justify-between">
-            <Link
-              href="/"
-              className="hover:text-base-foreground-weak text-2xl font-bold transition-colors duration-200 ease-out"
-              onClick={handleHomeLinkClick}
-            >
-              Yuta Hasegawa
-            </Link>
+    <header
+      ref={headerRef}
+      style={
+        {
+          "--header-border-bottom-width": hederBorderBottomWidth,
+        } as CSSProperties
+      }
+      className={cn(
+        "z-header sticky top-0",
+        "border-b-base-light-300 h-(--height-header) border-b-[length:var(--header-border-bottom-width)] border-solid bg-white",
+      )}
+    >
+      <Container>
+        <div className="relative flex h-[calc(var(--height-header)-var(--header-border-bottom-width))] items-center justify-between">
+          <Link
+            href="/"
+            className="hover:text-base-foreground-weak text-2xl font-bold transition-colors duration-200 ease-out"
+            onClick={handleHomeLinkClick}
+          >
+            Yuta Hasegawa
+          </Link>
 
-            {/* mobile only */}
-            <button
-              type="button"
-              ref={mobileMenuButtonRef}
-              className={cn(
-                "flex size-9 items-center justify-center sm:hidden",
-                "absolute top-1/2 -right-1.5 -translate-y-1/2",
-              )}
-              onClick={handleMobileMenuToggle}
-              aria-label={
-                isMobileMenuOpen ? "メニューを閉じる" : "メニューを開く"
-              }
-              aria-expanded={isMobileMenuOpen}
-              aria-controls={MOBILE_MENU_ID}
-            >
-              {isMobileMenuOpen ? (
-                <BsX aria-hidden="true" className="size-8" />
-              ) : (
-                <BsList aria-hidden="true" className="size-8" />
-              )}
-            </button>
+          {/* mobile only */}
+          <button
+            type="button"
+            ref={mobileMenuButtonRef}
+            className={cn(
+              "flex size-9 items-center justify-center sm:hidden",
+              "absolute top-1/2 -right-1.5 -translate-y-1/2",
+            )}
+            onClick={handleMobileMenuToggle}
+            aria-label={
+              isMobileMenuOpen ? "メニューを閉じる" : "メニューを開く"
+            }
+            aria-expanded={isMobileMenuOpen}
+            aria-controls={MOBILE_MENU_ID}
+          >
+            {isMobileMenuOpen ? (
+              <BsX aria-hidden="true" className="size-8" />
+            ) : (
+              <BsList aria-hidden="true" className="size-8" />
+            )}
+          </button>
 
-            {/* desktop only */}
-            <nav>
-              <ul className="hidden sm:flex">
-                {routesWithoutHome.map((route) => (
-                  <li key={route.href}>
-                    <ActiveLink
-                      href={route.href}
-                      className={cn(
-                        "relative inline-flex h-[calc(var(--height-header)-var(--header-border-bottom-width))] items-center px-4 font-bold transition-colors duration-200 ease-out",
-                        "hover:bg-base-light-100 active:bg-base-light-200",
-                        "data-active:before:bg-primary-600 data-active:before:absolute data-active:before:bottom-0 data-active:before:left-0 data-active:before:h-1 data-active:before:w-full data-active:before:content-['']",
-                      )}
-                    >
-                      {route.label}
-                    </ActiveLink>
-                  </li>
-                ))}
-              </ul>
-            </nav>
-          </div>
-        </Container>
+          {/* desktop only */}
+          <nav className="hidden sm:block">
+            <ul className="flex">
+              {routesWithoutHome.map((route) => (
+                <li key={route.href}>
+                  <ActiveLink
+                    href={route.href}
+                    className={cn(
+                      "relative inline-flex h-[calc(var(--height-header)-var(--header-border-bottom-width))] items-center px-4 font-bold transition-colors duration-200 ease-out",
+                      "hover:bg-base-light-100 active:bg-base-light-200",
+                      "data-active:before:bg-primary-600 data-active:before:absolute data-active:before:bottom-0 data-active:before:left-0 data-active:before:h-1 data-active:before:w-full data-active:before:content-['']",
+                    )}
+                  >
+                    {route.label}
+                  </ActiveLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+      </Container>
 
-        {/* mobile only */}
+      {/* mobile only */}
+      <nav className="sm:hidden">
         <ul
           id={MOBILE_MENU_ID}
           data-is-open={isMobileMenuOpen ? "true" : undefined}
           className={cn(
-            "sm:hidden",
             // height, visibilityを同時にtransitionで切り替えることで
             // 高さのアニメーションを適用しつつ、閉じているときにフォーカスも無効にする
             "absolute top-(--height-header) left-0 w-full overflow-y-hidden bg-white transition-[height,visibility] duration-200 ease-out",
@@ -165,7 +168,7 @@ export function Header() {
             </li>
           ))}
         </ul>
-      </div>
+      </nav>
     </header>
   );
 }
