b6fb3787c40445213d875c769c38b399aec5df62
Hasegawa Yuta <gssbtsm999@gmail.com>
2025-08-03 00:22:41 +0900

    chore: remove unused error handling components

--- src/lib/next/helpers.ts
+++
@@ -1,3 +0,0 @@
-export const isPageError404 = (error: Error) => {
-  return error.message === "fetch API response status: 404";
-};
--- src/lib/next/types.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Type of `error.tsx` props
- *
- * @see https://nextjs.org/docs/app/api-reference/file-conventions/error#props
- */
-export interface NextErrorProps {
-  error: Error & { digest?: string };
-  reset: () => void;
-}
