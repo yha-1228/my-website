a8f0d8138e67e6b9821b81dcecec95d7cac6fd30
Hasegawa Yuta <gssbtsm999@gmail.com>
2025-08-13 01:21:57 +0900

    refactor: streamline submitHubspotForm by removing unused response schema and simplifying response handling

--- src/actions/hubspot.ts
+++ src/actions/hubspot.ts
@@ -3,14 +3,11 @@
 import { z } from "zod";
 
 import { submitHubspotForm as _submitHubspotForm } from "@/api/endpoints/hubspot";
-import {
-  type SubmitHubspotFormRequestBody,
-  type SubmitHubspotFormResponse,
-} from "@/api/models/hubspot";
+import { type SubmitHubspotFormRequestBody } from "@/api/models/hubspot";
 
 export async function submitHubspotForm(
   requestBody: SubmitHubspotFormRequestBody,
-): Promise<SubmitHubspotFormResponse> {
+) {
   const portalId = z.string().min(1).parse(process.env.HUBSPOT_PORTAL_ID);
   const formGuid = z.string().min(1).parse(process.env.HUBSPOT_FORM_GUID);
 
--- src/api/endpoints/hubspot.ts
+++ src/api/endpoints/hubspot.ts
@@ -1,8 +1,4 @@
-import {
-  type SubmitHubspotFormRequestBody,
-  type SubmitHubspotFormResponse,
-  submitHubspotFormResponseSchema,
-} from "../models/hubspot";
+import { type SubmitHubspotFormRequestBody } from "../models/hubspot";
 
 /**
  * @see https://developers.hubspot.jp/docs/reference/api/marketing/forms/v3-legacy
@@ -11,7 +7,7 @@ export async function submitHubspotForm(
   portalId: string,
   formGuid: string,
   requestBody: SubmitHubspotFormRequestBody,
-): Promise<SubmitHubspotFormResponse> {
+) {
   const url = `https://api.hsforms.com/submissions/v3/integration/submit/${portalId}/${formGuid}`;
 
   const response = await fetch(url, {
@@ -20,20 +16,6 @@ export async function submitHubspotForm(
     body: JSON.stringify(requestBody),
   });
 
-  let json;
-
-  try {
-    json = await response.json();
-  } catch (error) {
-    console.log(`JSON parse error`, (error as Error).message);
-  }
-
-  const parsedResponse = submitHubspotFormResponseSchema.safeParse(json);
-
-  if (!parsedResponse.success) {
-    console.log(`Zod error`, parsedResponse.error.message);
-    throw new Error(`Parse error: ${parsedResponse.error.toString()}`);
-  }
-
-  return parsedResponse.data;
+  const json = await response.json();
+  return json;
 }
