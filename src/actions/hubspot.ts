04a1b0b0714508b33a831b94e24cbe060859f880
Hasegawa Yuta <gssbtsm999@gmail.com>
2025-08-14 21:31:14 +0900

    chore: remove console

--- src/actions/hubspot.ts
+++ src/actions/hubspot.ts
@@ -6,9 +6,6 @@ import { type SubmitHubspotFormRequestBody } from "@/api/models/hubspot";
 export async function submitHubspotForm(
   requestBody: SubmitHubspotFormRequestBody,
 ) {
-  console.log(`process.env.HUBSPOT_PORTAL_ID!`, process.env.HUBSPOT_PORTAL_ID!);
-  console.log(`process.env.HUBSPOT_FORM_GUID!`, process.env.HUBSPOT_FORM_GUID!);
-
   return _submitHubspotForm(
     process.env.HUBSPOT_PORTAL_ID!,
     process.env.HUBSPOT_FORM_GUID!,
