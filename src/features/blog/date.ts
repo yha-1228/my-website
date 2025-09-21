f2a884cddc29c82cfda2ab1cb3190d39c6c18b6b
Hasegawa Yuta <gssbtsm999@gmail.com>
2025-09-21 18:44:31 +0900

    fix: adjust date handling for production environment
    - Updated the date formatting function to account for a 9-hour offset in production by adding hours to the parsed date, ensuring accurate date representation.

--- src/features/blog/date.ts
+++ src/features/blog/date.ts
@@ -1,8 +1,14 @@
-import { format, parseISO } from "date-fns";
+import { addHours, format, parseISO } from "date-fns";
 
 export type DateTemplate = "yyyy年MM月dd日 HH:mm";
 
 export function dateFormat(template: DateTemplate, dateInput: string) {
-  const dateObj = parseISO(dateInput);
+  let dateObj = parseISO(dateInput);
+
+  // デプロイ後だと9時間後ろにずれるので対処する
+  if (process.env.NODE_ENV === "production") {
+    dateObj = addHours(dateObj, 9);
+  }
+
   return format(dateObj, template);
 }
