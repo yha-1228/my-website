1fd406ae88b6eca5140d4f17c50dac33e2a1541f
Hasegawa Yuta <gssbtsm999@gmail.com>
2025-09-25 11:03:02 +0900

    feat(experience): add description

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,8 +1,10 @@
 import { type Metadata } from "next";
+import Link from "next/link";
 import { Suspense } from "react";
 
 import { Container } from "@/components/ui/styled/container";
 import { Heading1 } from "@/components/ui/styled/heading1";
+import { TextLink } from "@/components/ui/styled/text-link";
 import { SITE_TITLE } from "@/constants";
 import { routes } from "@/routes";
 
@@ -17,8 +19,20 @@ export default function Page() {
     <div className="py-14">
       <Container className="md:max-w-(--breakpoint-md)">
         <section>
-          <div className="text-center">
+          <div className="flex flex-col gap-4 text-center">
             <Heading1>{routes.experience.label}</Heading1>
+            <p className="text-base-foreground-weak">
+              詳細なスキルシートについては
+              <TextLink
+                href={routes.contact.href}
+                as={Link}
+                className="text-primary-600 font-bold"
+                withUnderline
+              >
+                お問い合わせ
+              </TextLink>
+              からご連絡ください。
+            </p>
           </div>
           <Suspense>
             <ExperienceContent />
